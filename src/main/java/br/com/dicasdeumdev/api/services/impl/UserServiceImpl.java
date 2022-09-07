deb7ceec18e679cab189d18846773cbb4d6cad18
MarcosSouzaa <m.storeproducoes@gmail.com>
2022-09-07 15:38:53 -0300

    clase user service impl metodo findById

---
+++ src/main/java/br/com/dicasdeumdev/api/services/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package br.com.dicasdeumdev.api.services.impl;
+
+import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.repositories.UserRepository;
+import br.com.dicasdeumdev.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+    // Método FindById
+    @Override
+    public User findById(Integer id) {
+       Optional<User> obj = repository.findById(id); //Pode ser que encontre ou não, por isso é opcional
+        return obj.orElse(null); //retorna pra mim esse objeto, se não encontrar, retorna nulo
+    }
+}
