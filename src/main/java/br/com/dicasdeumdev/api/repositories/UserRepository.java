b236d6ae493ccaf6f2025823054abc0ad923a3ee
MarcosSouzaa <m.storeproducoes@gmail.com>
2022-09-07 15:36:18 -0300

    interface user repository

---
+++ src/main/java/br/com/dicasdeumdev/api/repositories/UserRepository.java
@@ -0,0 +1,50 @@
+package br.com.dicasdeumdev.api.repositories;
+
+import br.com.dicasdeumdev.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+                // Ele espera qual é a classe e o argumento que vamos persistir O nosso User(classe)
+                // Espera o tipo primitivo do obj identificador da entity e setamos como PK o Id e o tipo Integer
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
