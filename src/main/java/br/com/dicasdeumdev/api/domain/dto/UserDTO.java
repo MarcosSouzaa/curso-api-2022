7eb39fed1027e8ce67d883ca8b9830d666170ce0
MarcosSouzaa <m.storeproducoes@gmail.com>
2022-09-08 23:27:52 -0300

    classe user dto

---
+++ src/main/java/br/com/dicasdeumdev/api/domain/dto/UserDTO.java
@@ -0,0 +1,12 @@
+package br.com.dicasdeumdev.api.domain.dto;
+
+import javax.persistence.Column;
+
+public class UserDTO {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+}
+
